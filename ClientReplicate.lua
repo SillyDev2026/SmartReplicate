b55462fc69779ba11267d91095e2ac3d10616e18
SillyDev2026 <kramerlance797@gmail.com>
2025-09-23 03:23:45 -0800

    Create ClientReplicate.lua
    forgot ClientReplicate to work with so now it will work to help with Client to listen for Server and there is no need for Instances with this Smart Replicate

---
+++ ClientReplicate.lua
@@ -0,0 +1,117 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+local FolderSyncEvent = ReplicatedStorage:WaitForChild("FolderSyncEvent")
+
+local ActiveFolders = {}
+
+local function DeepCopy(tbl)
+	local copy = {}
+	for k,v in pairs(tbl) do
+		if type(v)=="table" then copy[k]=DeepCopy(v) else copy[k]=v end
+	end
+	return copy
+end
+
+local ClientFolder = {}
+ClientFolder.__index = ClientFolder
+
+function ClientFolder.new(userId, schema)
+	if ActiveFolders[userId] then return ActiveFolders[userId] end
+	local self = setmetatable({}, ClientFolder)
+	self.UserId = userId
+	self.Data = schema and DeepCopy(schema) or {}
+	self.Version = 1
+	self.ChangeListeners = {}
+	self.CreatedListeners = {}
+	ActiveFolders[userId] = self
+
+	for folderName,_ in pairs(self.Data) do
+		self:FireCreated(folderName)
+	end
+
+	return self
+end
+
+function ClientFolder:EnsureFolder(folderName)
+	if not self.Data[folderName] then
+		self.Data[folderName] = {}
+		self:FireCreated(folderName)
+	end
+end
+
+function ClientFolder:FireCreated(folderName)
+	for _,cb in ipairs(self.CreatedListeners) do
+		cb(folderName)
+	end
+end
+
+function ClientFolder:OnCreated(callback)
+	table.insert(self.CreatedListeners, callback)
+end
+
+function ClientFolder:FireChanged(path, newValue, oldValue)
+	if self.ChangeListeners[path] then
+		for _, cb in ipairs(self.ChangeListeners[path]) do
+			cb(newValue, oldValue)
+		end
+	end
+end
+
+function ClientFolder:OnChanged(path, callback)
+	self.ChangeListeners[path] = self.ChangeListeners[path] or {}
+	table.insert(self.ChangeListeners[path], callback)
+end
+
+function ClientFolder:ApplyUpdate(folderName, itemName, value)
+	self:EnsureFolder(folderName)
+	local folder = self.Data[folderName]
+
+	local oldValue = folder[itemName]
+	folder[itemName] = value
+	self.Version += 1
+	self:FireChanged(folderName.."."..itemName, value, oldValue)
+end
+
+function ClientFolder:RequestUpdate(folderName, itemName, value)
+	FolderSyncEvent:FireServer("RequestUpdate", folderName, itemName, value)
+end
+
+function ClientFolder.get(userId)
+	local folder = ActiveFolders[userId]
+	while not folder do
+		task.wait()
+		folder = ActiveFolders[userId]
+	end
+	return folder
+end
+
+function ClientFolder.remove(userId)
+	ActiveFolders[userId] = nil
+end
+
+FolderSyncEvent.OnClientEvent:Connect(function(action, userId, ...)
+	if action == "InitPlayer" then
+		local data = ...
+		ClientFolder.new(userId, data)
+	elseif action == "Update" then
+		local folderName, itemName, value = ...
+		local folder = ClientFolder.get(userId)
+		if folder then
+			folder:ApplyUpdate(folderName, itemName, value)
+		end
+	elseif action == "RemovePlayer" then
+		ClientFolder.remove(userId)
+	end
+end)
+
+function ClientFolder:RequestModule(moduleName, action)
+	FolderSyncEvent:FireServer('RequestModule', moduleName, action)
+end
+
+function ClientFolder:GetData(folderName, statName)
+	return self.Data[folderName][statName]
+end
+
+return ClientFolder
