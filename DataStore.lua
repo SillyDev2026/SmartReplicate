6b7c81bdfe3f57184ee0db9d7d698f2ac30e188f
SillyDev2026 <kramerlance797@gmail.com>
2026-03-10 01:19:25 -0800

    Update DataStore.lua

--- DataStore.lua
+++ DataStore.lua
@@ -3,23 +3,21 @@ local Module = script.VirtualData
 local VirtualData = require(Module)
 local Modules = script.Modules
 local DataStore = require(Modules:FindFirstChild('DataStore'))
-local Store = DataStore.new({Name = 'LevelTracker'})
-local PlayersData = script.PlayersData
-
-function Data(name)
-	return require(PlayersData[name])
-end
+local StoreName = script.DataStoreName.Value
+local Store = DataStore.new({Name = script.DataStoreName.Value})
+local PlayersData = script:FindFirstChild(StoreName)
 
 Players.PlayerAdded:Connect(function(player)
 	local key = Store.SetKey(player)
 	local default = {}
 	for _, modules in pairs(PlayersData:GetChildren()) do
-		if modules:IsA('ModuleScript') then
+		if modules:IsA('ModuleScript') and (modules.Name ~= 'DataStore' and modules.Name ~= 'PlayersDataStore') then
 			default[modules.Name] = require(modules)
 		end
 	end	
 	local data = Store:GetAsync(key, player, default)
-	VirtualData.new(player, data, Store)
+	local folder = VirtualData.new(player, data, Store)
+	-- u create the rest here if any problems just contact sillydev0050 in discord
 end)
 
 Players.PlayerRemoving:Connect(function(player)
